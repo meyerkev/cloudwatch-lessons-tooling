48e1e58d247d2310d6cf255295b499a3a3ce470d
Kevin Meyer <meyerkev248@gmail.com>
2023-03-25 19:37:25 -0400

    Adding a custom metric

--- js/logger.js
+++ js/logger.js
@@ -1,8 +1,50 @@
 // Write a simple lambda function that logs the event to the console.
+const { CloudWatchClient, PutMetricDataCommand } = require("@aws-sdk/client-cloudwatch");
 
-exports.handler = (event, context, callback) => {
-    console.log(event);
-    callback(null, event);
-    // Print the current time
-    console.log("Current time is: " + new Date());
+exports.handler = async (event, context, callback) => {
+    try{
+        console.log(event);
+        callback(null, event);
+        // Print the current time
+        var timestamp = new Date();
+        console.log("Current time is: " + timestamp);
+
+        // Write to a custom Cloudwatch metric
+        console.log("Reporting to Cloudwatch");
+        
+
+        
+        const cloudwatch = new CloudWatchClient({});
+
+
+        // Create the parameters for the metric
+        // Define the custom metric namespace and name
+        const namespace = 'CloudWatchClassSampleNamespace';
+        const metricName = 'CloudWatchClassSampleMetric';
+        const metricData = {
+            MetricData: [
+            {
+                MetricName: metricName,
+                Dimensions: [
+                {
+                    Name: 'SampleDimension',
+                    Value: 'SampleDimensionValue'
+                },
+                ],
+                Timestamp: timestamp,
+                Unit: 'Count',
+                Value: 1
+            },
+            ],
+            Namespace: namespace
+        };
+     
+        const data = await cloudwatch.send(new PutMetricDataCommand(metricData));
+        console.log("Success:", JSON.stringify(data));
+    } catch (err) {
+        console.log("Error:", err);
+        throw err;
+    }
 };
+
+//exports.handler({}, {}, (err, data) => {});
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,61 @@
 # use our lamdba module
 
 module "lambda_function" {
-  source = "terraform-aws-modules/lambda/aws"
+    source = "terraform-aws-modules/lambda/aws"
 
-  function_name = "cloudwatch-class-lambda"
-  description   = "A simple logging lambda function"
-  handler       = "logger.handler"
-  runtime       = "nodejs18.x"
+    function_name = "cloudwatch-class-lambda"
+    description   = "A simple logging lambda function"
+    handler       = "logger.handler"
+    runtime       = "nodejs18.x"
+    timeout = 10
 
-  source_path = "../js/logger.js"
+    source_path = "../js/logger.js"
 
-  tags = {}
+    tags = {}
+
+    store_on_s3 = true
+    s3_bucket = module.s3_bucket.s3_bucket_id
+
+    attach_policies = true
+    policies =  [
+        aws_iam_policy.lambda_cloudwatch_policy.arn
+    ]
+    number_of_policies = 1
+}
+
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "cloudwatch-class-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+resource "aws_iam_policy" "lambda_cloudwatch_policy" {
+  name = "lambda_cloudwatch_policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ]
+        Resource = "arn:aws:logs:*:*:*"
+        Effect = "Allow"
+      },
+      {
+        Action = [
+          "cloudwatch:PutMetricData"
+        ]
+        Resource = "*"
+        Effect = "Allow"
+      }
+    ]
+  })
 }
